b42000edc6f1b4a424c91d31017b6ba9f48a2842
cr0sh <ska827@naver.com>
2016-05-24 20:52:16 +0900

    Improved BF-MF conversion(WIP)

--- format.go
+++ format.go
@@ -9,6 +9,7 @@
 //
 // MF 바이너리의 첫 4바이트는 Magic(\xff\x6d\x66\xfd),
 // 다음 32비트는 할당할 VM 메모리 크기입니다.
+// BF에서 MF로 강제 변환한 코드의 경우 Magic은 \xff\x6d\x68\xfd입니다.
 //
 // 각 BF 코드 1바이트는 MF 코드 1니블로 치환됩니다.
 //  +: 0
@@ -19,6 +20,10 @@
 //  ]: 5
 //  .: 6
 //  ,: 7
+//
+// 주의: [, ] 즉 4, 5 코드는 special code로 대체됩니다. non-special code에서의 4, 5 코드 구현은 표준에서도 정의하지 않습니다.
+// 레거시 호환성을 위해 구현할 수 있으나, special code의 4, 5 사용을 권장합니다.
+//
 // 니블코드의 첫 비트가 1(전체 니블이 8 이상)인 경우, 뒤 3비트는 special code로 취급되어 특수 목적으로 사용됩니다.
 //
 // 주의: 홀수 번째 니블에 special code가 있다면, 뒤 니블은 버려집니다.
@@ -44,8 +49,11 @@
 package mf
 
 import (
+	"bytes"
+	"encoding/hex"
 	"fmt"
 	"io"
+	"strings"
 )
 
 // DefaultMemSize defines default memory size allocated
@@ -55,22 +63,25 @@ const DefaultMemSize uint32 = 1024
 // Magic is a magic bytes for MF binary file.
 const Magic = "\xff\x6d\x66\xfd"
 
+// BFMagic is a magic bytes for BF-converted MF binary file.
+const BFMagic = "\xff\x6d\x68\xfd"
+
 const bf = "+-><[].,"
 
 // ToBF will accept MF code with Write function,
 // and write to wrapping Writer interface.
 type ToBF struct {
 	wr      io.Writer
-	rdSize  uint
+	rdSize  uint32
 	misc    [4]byte // magic, memsize storage
 	memSize uint32  // at least 32-bit
 	sbit    bool    // special bit flag
 	scode   byte    // special code
-	rdGoal  byte    // bytes limit to read compressed length
+	rdGoal  uint32  // bytes limit to read compressed length
 }
 
-// NewReader returns new mf.ToBF struct.
-func NewReader(wr io.Writer) *ToBF {
+// NewBFWriter returns new mf.ToBF struct.
+func NewBFWriter(wr io.Writer) *ToBF {
 	return &ToBF{wr: wr}
 }
 
@@ -89,13 +100,14 @@ func (r *ToBF) Write(p []byte) (n int, err error) {
 			if r.rdSize != 8 {
 				r.misc[r.rdSize-4] = b
 			} else {
+				r.wr.Write([]byte("MinFuck compiled code\n"))
 				r.allocMem(r.miscData())
 			}
 		case r.rdSize <= r.rdGoal:
 			r.misc[(r.rdSize+3)-r.rdGoal] = b
 			if r.rdSize == r.rdGoal {
 				for i := r.miscData(); i > 0; i-- {
-					r.wr.Write([]byte{bf[r.scode]})
+					r.wr.Write([]byte(bf[r.scode : r.scode+1]))
 				}
 			}
 		default:
@@ -107,7 +119,7 @@ func (r *ToBF) Write(p []byte) (n int, err error) {
 				case r.scode < 4: // compressed code
 					r.rdGoal = r.rdSize + 4
 				case r.scode == 4 || r.scode == 5:
-					r.wr.Write([]byte{bf[r.scode]})
+					r.wr.Write([]byte(bf[r.scode : r.scode+1]))
 				case r.scode == 6:
 					r.sbit = false // no-op
 				}
@@ -139,10 +151,183 @@ func (r *ToBF) processNibble(n byte) {
 	r.wr.Write([]byte{bf[n]})
 }
 
-func (r *ToBf) miscData() uint32 {
+func (r *ToBF) miscData() uint32 {
 	return uint32(r.misc[0])<<24 | uint32(r.misc[1])<<12 | uint32(r.misc[2])<<8 | uint32(r.misc[3])
 }
 
 func (r *ToBF) allocMem(size uint32) {
-	//TODO
+	r.wr.Write([]byte(">>+>>+>>+>>+>"))
+	r.wr.Write([]byte(strings.Repeat("+", int(size))))
+	r.wr.Write([]byte("[[->>+<<]>+>-]<[<<]"))
+}
+
+// FromBF converts BF code to MF, and writes to the wrapping Writer.
+type FromBF struct {
+	wr   *bytes.Buffer
+	wrap io.Writer
+	buf  byte
+	last byte
+	dup  uint32
+	half bool
+}
+
+// NewBFWriter returns new FromBF struct.
+func NewBFReader(wr io.Writer, memsize uint32) *FromBF {
+	r := new(FromBF)
+	r.wr = new(bytes.Buffer)
+	r.wrap = wr
+	r.wr.Write([]byte(BFMagic))
+	r.wr.Write(uint32bytes(memsize))
+	return r
+}
+
+// Write implements io.Writer interface.
+func (r *FromBF) Write(p []byte) (n int, err error) {
+	for _, b := range p {
+		switch b {
+		case 43, 45, 62, 60:
+			var t byte
+			switch b {
+			case 43:
+				t = 0
+			case 45:
+				t = 1
+			case 62:
+				t = 2
+			case 60:
+				t = 3
+			}
+			if t != r.last {
+				r.clearDup()
+				switch b {
+				case 43:
+					r.last = 0
+				case 45:
+					r.last = 1
+				case 62:
+					r.last = 2
+				case 60:
+					r.last = 3
+				}
+				r.dup = 1
+			} else {
+				r.dup++
+			}
+		case 91, 93:
+			if r.dup > 0 {
+				r.clearDup()
+			}
+			if b == 91 {
+				r.writeNibble(8 | 4)
+			} else {
+				r.writeNibble(8 | 5)
+			}
+			if r.half {
+				r.writeNibble(8 | 6)
+			}
+			r.wr.Write(make([]byte, 4))
+		case 46, 44:
+			if r.dup > 0 {
+				r.clearDup()
+			}
+			if b == 46 {
+				r.writeNibble(6)
+			} else {
+				r.writeNibble(7)
+			}
+		}
+	}
+	return len(p), nil
+}
+
+func (r *FromBF) clearDup() {
+	if r.dup > 9 {
+		r.writeNibble(8 | r.last)
+		if r.half {
+			r.writeNibble(14)
+		}
+		r.wr.Write(uint32bytes(r.dup))
+	} else {
+		for i := uint32(0); i < r.dup; i++ {
+			r.writeNibble(r.last)
+		}
+	}
+	r.dup = 0
+}
+
+func (r *FromBF) writeNibble(p byte) error {
+	if r.half {
+		r.half = false
+		_, err := r.wr.Write([]byte{r.buf | (p & 0xf)})
+		if err != nil {
+			panic(err.Error())
+		}
+		return err
+	} else {
+		r.half, r.buf = true, (p&0xf)<<4
+		return nil
+	}
+}
+
+// Close implements io.Closer interface.
+func (r *FromBF) Close() error {
+	if r.dup > 0 {
+		r.clearDup()
+	}
+	r.cacheJumpOff()
+	io.Copy(r.wrap, r.wr)
+	return nil
+}
+
+func (r *FromBF) cacheJumpOff() {
+	s := new(stack)
+	s.mem = make([]uint32, 1024)
+	buf := r.wr.Bytes()
+	fmt.Print(hex.Dump(buf))
+	for i := 8; i < len(buf); i++ {
+		b := buf[i]
+		n1, n2 := b>>4, b&0xf
+		if n1 == 0xc || n2 == 0xc {
+			s.put(uint32(i))
+			i += 4
+		} else if n1 == 0xd || n2 == 0xd {
+			jmp := s.get()
+			fmt.Printf("Loop index pair %2x %2x\n", jmp, i)
+			copy(buf[i+1:i+5], uint32bytes(jmp+5))
+			copy(buf[jmp+1:jmp+5], uint32bytes(uint32(i)+5))
+			i += 4
+		}
+	}
+	fmt.Print(hex.Dump(buf))
+	r.wrap.Write(buf)
+}
+
+type stack struct {
+	mem []uint32
+	off int
+}
+
+func (s *stack) put(n uint32) {
+	if len(s.mem) <= s.off {
+		s.mem = append(s.mem, make([]uint32, len(s.mem))...)
+	}
+	s.mem[s.off] = n
+	s.off++
+}
+
+func (s *stack) get() uint32 {
+	s.off--
+	if s.off < 0 {
+		panic("invalid stack pointer: tried to get value from empty stack")
+	}
+	return s.mem[s.off]
+}
+
+func uint32bytes(n uint32) []byte {
+	return []byte{
+		byte(n >> 24),
+		byte(n >> 16),
+		byte(n >> 8),
+		byte(n),
+	}
 }
--- main.go
+++ main.go
@@ -2,6 +2,61 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"os"
+	"path"
 
+	"github.com/cr0sh/mf"
+)
+
+const help = `
+MF-tools v1.0
+
+Command usage:
+m2b: convert MF to BF
+b2m: convert BF to MF
+`
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println(help)
+		return
+	}
+	cmd := os.Args[1]
+	switch cmd {
+	case "m2b":
+		fp, err := os.Create(os.Args[2][0:len(os.Args[2])-len(path.Ext(os.Args[2]))] + ".b")
+		if err != nil {
+			fmt.Println("error:", err)
+			return
+		}
+		fpp, err := os.Open(os.Args[2])
+		if err != nil {
+			fmt.Println("error:", err)
+			return
+		}
+		r := mf.NewBFWriter(fp)
+		io.Copy(r, fpp)
+		fpp.Close()
+
+	case "b2m":
+		fp, err := os.Create(os.Args[2][0:len(os.Args[2])-len(path.Ext(os.Args[2]))] + ".mf")
+		if err != nil {
+			fmt.Println("error:", err)
+			return
+		}
+		fpp, err := os.Open(os.Args[2])
+		if err != nil {
+			fmt.Println("error:", err)
+			return
+		}
+		r := mf.NewBFReader(fp, 4096)
+		io.Copy(r, fpp)
+		r.Close()
+		fpp.Close()
+	default:
+		fmt.Println(help)
+	}
 }
